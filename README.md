4eab97e798df1b7316c0bc917c4e52535c15928d
Jamen Lyndon <jamenlyndon@everydayhero.com>
2015-05-25 13:21:13 +1000

    Added JS, updated Read Me

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # Parallax-Scrolling
 Vanilla JS parallax scrolling function
+
+## Version:
+1.0
+
+## Params:
+```
+- elementId (str): The element's ID.
+- backgroundWidth (int): Background image's width in pixels.
+- backgroundHeight (int): Background image's height in pixels.
+- scrollDirectionX (str): Optional. Set to either "left" or "right" to specify horizontal scroll direction.
+- scrollDirectionY (str): Optional. Set to either "up" or "down" to specify vertical scroll direction.
+- maxOffsetX (int): Optional. Max number of pixels the background can move horizontally. Set this to 0 to disable horizontal scrolling.
+- maxOffsetY (int): Optional. Max number of pixels the background can move vertically. Set this to 0 to disable vertical scrolling.
+```
+
+## Browser Support:
+IE9+, Chrome, Firefox, Safari, Opera (Should support IE8 as well, but this is untested).
+
+## Known Bugs:
+The scroll event on mobile only returns after it stops scrolling. Possible fix: https://filipbech.github.io/2013/07/smooth-parallax-scrolling-on-ios-with/
+
+## Example Usage:
+HTML
+```
+<div id="header"></div>
+```
+
+CSS
+```
+#header {
+  background: url('img/example.jpg') no-repeat;
+  background-size: cover;
+
+  width: 1024px;
+  height: 400px;
+}
+```
+
+JS
+```
+function doParallax() {
+  var pos = parallaxScroll('header', 1024, 768);
+  document.getElementById('header').style.backgroundPosition = (pos[0]+'px '+pos[1]+'px');
+}
+
+$(document).ready(doParallax);
+$(window).scroll(doParallax);
+$(window).resize(doParallax);
+```
---
+++ parallaxScrolling-1.0.js
@@ -0,0 +1,105 @@
+function parallaxScroll(elementId, backgroundWidth, backgroundHeight, scrollDirectionX, scrollDirectionY, maxOffsetX, maxOffsetY) {
+  // Set optional params
+  if (typeof scrollDirection == 'undefined') {
+    scrollDirection = 'down';
+  }
+
+  if (typeof maxOffsetX == 'undefined') {
+    maxOffsetX = 'auto';
+  }
+  else {
+    maxOffsetX = maxOffsetX * -1;
+  }
+
+  if (typeof maxOffsetY == 'undefined') {
+    maxOffsetY = 'auto';
+  }
+  else {
+    maxOffsetY = maxOffsetY * -1;
+  }
+
+
+  // Setup variables
+  var obj = document.getElementById(elementId),
+
+  w = window,
+  d = document,
+  e = d.documentElement,
+  g = d.getElementsByTagName('body')[0],
+
+  windowHeight = w.innerHeight|| e.clientHeight|| g.clientHeight,
+  windowWidth = w.innerWidth || e.clientWidth || g.clientWidth,
+
+  scrollTop = w.pageYOffset || e.scrollTop,
+  scrollBottom = scrollTop + windowHeight,
+  scrollLeft = w.pageXOffset || e.scrollLeft,
+  scrollRight = scrollLeft + windowWidth,
+
+  containerWidth = obj.offsetWidth,
+  containerHeight = obj.offsetHeight,
+
+  containerTop = obj.offsetTop,
+  containerBottom = containerTop + containerHeight,
+  containerLeft = obj.offsetLeft,
+  containerRight = containerLeft + containerWidth;
+
+
+  // Is this image using "cover"? Set the background to the right ratio.
+  var style = w.getComputedStyle(obj),
+      bgSize = style.getPropertyValue('background-size');
+
+  if (bgSize=='cover') {
+    // Get the ratio of the div & the image
+    var imageRatio = backgroundWidth / backgroundHeight,
+        coverRatio = containerWidth / containerHeight;
+
+    // Work out which ratio is greater
+    if (imageRatio >= coverRatio) {
+      var coverHeight = containerHeight,
+          scale = (coverHeight / backgroundHeight),
+          coverWidth = backgroundWidth * scale;
+    } else {
+      var coverWidth = containerWidth,
+          scale = (coverWidth / backgroundWidth),
+          coverHeight = backgroundHeight * scale;
+    }
+
+    backgroundWidth = coverWidth;
+    backgroundHeight = coverHeight;
+  }
+
+
+  // Calculate the HORIZONTAL background position
+  var xPos = 0;
+  if (scrollRight >= containerLeft && scrollLeft <= containerRight) {
+    var percentageOfTotalScroll = 100 - (((containerRight - scrollLeft) / containerRight) * 100);
+
+    if (scrollDirectionX=='up') {
+      percentageOfTotalScroll = 100 - percentageOfTotalScroll;
+    }
+    var totalOffset = containerWidth - backgroundWidth;
+    if (maxOffsetX != 'auto' && totalOffset < maxOffset) {
+      totalOffset = maxOffsetX;
+    }
+    xPos = totalOffset * (percentageOfTotalScroll / 100);
+  }
+
+
+  // Calculate the VERTICAL background position
+  var yPos = 0;
+  if (scrollBottom >= containerTop && scrollTop <= containerBottom) {
+    var percentageOfTotalScroll = 100 - (((containerBottom - scrollTop) / containerBottom) * 100);
+
+    if (scrollDirectionY=='up') {
+      percentageOfTotalScroll = 100 - percentageOfTotalScroll;
+    }
+    var totalOffset = containerHeight - backgroundHeight;
+    if (maxOffsetY != 'auto' && totalOffset < maxOffset) {
+      totalOffset = maxOffsetY;
+    }
+    yPos = totalOffset * (percentageOfTotalScroll / 100);
+  }
+
+
+  return [xPos, yPos];
+}
