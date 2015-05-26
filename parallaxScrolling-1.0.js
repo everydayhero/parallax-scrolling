3f3ce5704c981a36843e508759aa5f5908b7e932
Jamen Lyndon <jamenlyndon@everydayhero.com>
2015-05-26 11:51:02 +1000

    Refactor code

--- parallaxScrolling-1.0.js
+++ parallaxScrolling-1.0.js
@@ -1,153 +1,167 @@
-function parallaxInit(elementId, background, scrollDirectionX, scrollDirectionY, maxOffsetX, maxOffsetY) {
-  // Set optional params
-  if (typeof scrollDirectionX == 'undefined') {
-    scrollDirectionX = 'right';
-  }
-  if (typeof scrollDirectionY == 'undefined') {
-    scrollDirectionY = 'down';
-  }
+(function() {
+  var Parallax = window.Parallax = (function() {
 
-  if (typeof maxOffsetX == 'undefined') {
-    maxOffsetX = 'auto';
-  }
-  else {
-    maxOffsetX = maxOffsetX * -1;
-  }
+    var params = {};
+    var state = {};
 
-  if (typeof maxOffsetY == 'undefined') {
-    maxOffsetY = 'auto';
-  }
-  else {
-    maxOffsetY = maxOffsetY * -1;
-  }
+    var initialised = false;
 
+    // Init function
+    // ------------------------------------------------
+    // id (str): Required. ID of DOM element.
+    // src (str): Required. ID of DOM element.
+    // scrollDir (str): Optional. Scroll direction can be either "down" or "up". Default is "down".
+    // maxOffset (int): Optional. Max number of pixels the background can move vertically. By default it will use all available overflowing pixels.
+    this.init = function(id, src, scrollDir, maxOffset) {
 
-  // Setup variables
-  var obj = document.getElementById(elementId),
+      // Make sure it only fires once
+      if (initialised) {
+        return false;
+      }
 
-  containerWidth = obj.offsetWidth,
-  containerHeight = obj.offsetHeight;
+      // Set optional params
+      var scrollDir = scrollDir || 'down';
+      var maxOffset = (maxOffset * -1) || 'auto';
 
+      // Setup state variables
+      var el = document.getElementById(id);
+      var w = window;
+      var d = document;
+      var e = d.documentElement;
+      var g = d.getElementsByTagName('body')[0]; // For older browsers
 
-  // Create the background image
-  var backgroundImg = new Image();
-  backgroundImg.src = background;
+      // Add variables to state
+      state = {
+        'el': el,
+        'w': w,
+        'd': d,
+        'e': e,
+        'g': g
+      };
 
-  // Once it's loaded
-  backgroundImg.onload = function() {
-    // Set width and height
-    backgroundWidth = backgroundImg.width;
-    backgroundHeight = backgroundImg.height;
+      // Create the background image
+      var backgroundImg = new Image();
+      backgroundImg.src = src;
 
-    // Apply to div
-    obj.style.backgroundImage = "url('"+background+"')";
+      // Once it's loaded
+      backgroundImg.onload = function() {
+        // Set width and height
+        backgroundWidth = backgroundImg.width;
+        backgroundHeight = backgroundImg.height;
 
-    // Output arguments to global variable
-    parallaxArgs[elementId] = [];
-    parallaxArgs[elementId]['backgroundWidth'] = backgroundWidth;
-    parallaxArgs[elementId]['backgroundHeight'] = backgroundHeight;
-    parallaxArgs[elementId]['scrollDirectionX'] = scrollDirectionX;
-    parallaxArgs[elementId]['scrollDirectionY'] = scrollDirectionY;
-    parallaxArgs[elementId]['maxOffsetX'] = maxOffsetX;
-    parallaxArgs[elementId]['maxOffsetY'] = maxOffsetY;
+        // Apply image to DOM element
+        state.el.style.backgroundImage = "url('"+src+"')";
 
-    // Trigger the scroll
-    parallaxScroll(elementId);
-  }
+        // Add variables to state
+        state = {
+          'backgroundWidth': backgroundWidth,
+          'backgroundHeight': backgroundHeight
+        };
 
-}
+        // Add variables to params
+        params = {
+          'id': id,
+          'scrollDir': scrollDir,
+          'maxOffset': maxOffset
+        };
 
+        // Trigger the resize to set all state vars (resize also calls scroll)
+        return Parallax.resize();
+      }
 
-
-function parallaxScroll(elementId) {
-  // Setup variables
-  var paramsArr = parallaxArgs[elementId];
-
-  // If the images aren't loaded yet, the global vars won't be set
-  if (typeof paramsArr == 'undefined') {
-    return false;
-  }
-
-  var obj = document.getElementById(elementId),
-
-  w = window,
-  d = document,
-  e = d.documentElement,
-  g = d.getElementsByTagName('body')[0],
-
-  windowHeight = w.innerHeight|| e.clientHeight|| g.clientHeight,
-  windowWidth = w.innerWidth || e.clientWidth || g.clientWidth,
-
-  scrollTop = w.pageYOffset || e.scrollTop,
-  scrollBottom = scrollTop + windowHeight,
-  scrollLeft = w.pageXOffset || e.scrollLeft,
-  scrollRight = scrollLeft + windowWidth,
-
-  containerWidth = obj.offsetWidth,
-  containerHeight = obj.offsetHeight,
-
-  containerTop = obj.offsetTop,
-  containerBottom = containerTop + containerHeight,
-  containerLeft = obj.offsetLeft,
-  containerRight = containerLeft + containerWidth;
+      initialised = true;
+    };
+  });
 
 
-  // Is this image using "cover"? Set the background to the right ratio.
-  var style = window.getComputedStyle(obj),
-      bgSize = style.getPropertyValue('background-size');
 
-  if (bgSize=='cover') {
-    // Get the ratio of the div & the image
-    var imageRatio = paramsArr['backgroundWidth'] / paramsArr['backgroundHeight'],
-        coverRatio = containerWidth / containerHeight;
-
-    // Work out which ratio is greater
-    if (imageRatio >= coverRatio) {
-      var coverHeight = containerHeight,
-          scale = (coverHeight / paramsArr['backgroundHeight']),
-          coverWidth = paramsArr['backgroundWidth'] * scale;
+  Parallax.prototype.scroll = function() {
+    // If the images aren't loaded yet, the args var won't be set so return false
+    if (Object.keys(args).length==0) {
+      return false;
     } else {
-      var coverWidth = containerWidth,
-          scale = (coverWidth / paramsArr['backgroundWidth']),
-          coverHeight = paramsArr['backgroundHeight'] * scale;
+      // Setup state variables
+      var scrollTop = w.pageYOffset || e.scrollTop;
+      var scrollBottom = scrollTop + windowHeight;
+      var containerWidth = obj.offsetWidth;
+      var containerHeight = obj.offsetHeight;
+      var containerTop = obj.offsetTop;
+      var containerBottom = containerTop + containerHeight;
+
+      // Add variables to state
+      state.scrollTop = scrollTop;
+      state.scrollBottom = scrollBottom;
+      state.containerWidth = containerWidth;
+      state.containerHeight = containerHeight;
+      state.containerTop = containerTop;
+      state.containerBottom = containerBottom;
+
+      // Calculate the VERTICAL background position
+      var yPos = 0;
+      if (state.scrollBottom >= state.containerTop && state.scrollTop <= state.containerBottom && (params.maxOffset != 0 || params.maxOffset == 'auto')) {
+        var percentageOfTotalScroll = 100 - (((state.containerBottom - state.scrollTop) / state.containerBottom) * 100);
+
+        if (params.scrollDirectionY=='up') {
+          percentageOfTotalScroll = 100 - percentageOfTotalScroll;
+        }
+        var totalOffset = state.containerHeight - params.backgroundHeight;
+        if (params.maxOffset != 'auto' && totalOffset < params.maxOffset) {
+          totalOffset = params.maxOffsetY;
+        }
+        yPos = totalOffset * (percentageOfTotalScroll / 100);
+      }
+
+      // Apply the background position
+      // obj.style.backgroundPosition = ('0 '+yPos+'px');
+
+      return yPos;
     }
-
-    paramsArr['backgroundWidth'] = coverWidth;
-    paramsArr['backgroundHeight'] = coverHeight;
-  }
+  };
 
 
-  // Calculate the HORIZONTAL background position
-  var xPos = 0;
-  if (scrollRight >= containerLeft && scrollLeft <= containerRight && (paramsArr['maxOffsetX'] != 0 || paramsArr['maxOffsetX'] == 'auto')) {
-    var percentageOfTotalScroll = 100 - (((containerRight - scrollLeft) / containerRight) * 100);
 
-    if (paramsArr['scrollDirectionX'] == 'up') {
-      percentageOfTotalScroll = 100 - percentageOfTotalScroll;
+  Parallax.prototype.resize = function() {
+    // If the images aren't loaded yet, the args var won't be set so return false
+    if (Object.keys(args).length==0) {
+      return false;
+    } else {
+      // Setup state variables
+      var windowHeight = state.w.innerHeight || state.e.clientHeight || state.g.clientHeight;
+      var windowWidth = state.w.innerWidth || state.e.clientWidth || state.g.clientWidth;
+
+      // Is this image using "cover"? Set the background to the right ratio.
+      var style = state.w.getComputedStyle(state.el);
+      var bgSize = style.getPropertyValue('background-size');
+
+      if (bgSize=='cover') {
+        // Get the ratio of the div & the image
+        var imageRatio = state.backgroundWidth / state.backgroundHeight;
+        var coverRatio = state.containerWidth / state.containerHeight;
+
+        // Work out which ratio is greater
+        if (imageRatio >= coverRatio) {
+          var coverHeight = state.containerHeight;
+          var scale = (coverHeight / state.backgroundHeight);
+          var coverWidth = state.backgroundWidth * scale;
+        } else {
+          var coverWidth = state.containerWidth;
+          var scale = (coverWidth / state.backgroundWidth);
+          var coverHeight = state.backgroundHeight * scale;
+        }
+
+        // Add variables to state
+        state.backgroundWidth = coverWidth;
+        state.backgroundHeight = coverHeight;
+      }
+
+      // Add variables to state
+      state.windowHeight = windowHeight;
+      state.windowWidth = windowWidth;
+
+      // Call scroll function
+      return Parallax.scroll();
     }
-    var totalOffset = containerWidth - paramsArr['backgroundWidth'];
-    if (paramsArr['maxOffsetX'] != 'auto' && totalOffset < paramsArr['maxOffsetX']) {
-      totalOffset = paramsArr['maxOffsetX'];
-    }
-    xPos = totalOffset * (percentageOfTotalScroll / 100);
-  }
+  };
 
-
-  // Calculate the VERTICAL background position
-  var yPos = 0;
-  if (scrollBottom >= containerTop && scrollTop <= containerBottom && (paramsArr['maxOffsetY'] != 0 || paramsArr['maxOffsetY'] == 'auto')) {
-    var percentageOfTotalScroll = 100 - (((containerBottom - scrollTop) / containerBottom) * 100);
-
-    if (paramsArr['scrollDirectionY']=='up') {
-      percentageOfTotalScroll = 100 - percentageOfTotalScroll;
-    }
-    var totalOffset = containerHeight - paramsArr['backgroundHeight'];
-    if (paramsArr['maxOffsetY'] != 'auto' && totalOffset < paramsArr['maxOffsetY']) {
-      totalOffset = paramsArr['maxOffsetY'];
-    }
-    yPos = totalOffset * (percentageOfTotalScroll / 100);
-  }
-
-  // Apply the background position
-  obj.style.backgroundPosition = (xPos+'px '+yPos+'px');
-}
+  return Parallax;
+})();
